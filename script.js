36808d1ff8475540e628cb87aa288acd0fa1a6ca
stor73140 <stor73140@gmail.com>
2026-03-06 10:31:22 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,115 @@
+const API_URL="COLLER_URL_GOOGLE_SCRIPT"
+
+let data=[]
+
+async function loadData(){
+
+let res=await fetch(API_URL)
+data=await res.json()
+
+display()
+
+}
+
+function display(){
+
+let search=document.getElementById("search").value.toLowerCase()
+
+let table=document.getElementById("table")
+table.innerHTML=""
+
+data.forEach(p=>{
+
+if(!p.piece.toLowerCase().includes(search)) return
+
+table.innerHTML+=`
+<tr>
+<td>${p.gare}</td>
+<td>${p.piece}</td>
+<td>${p.qte}</td>
+<td>${p.date}</td>
+</tr>
+`
+
+})
+
+buildGeneral()
+
+}
+
+function buildGeneral(){
+
+let stock={}
+
+data.forEach(p=>{
+
+if(!stock[p.piece]) stock[p.piece]=0
+stock[p.piece]+=Number(p.qte)
+
+})
+
+let table=document.getElementById("generalTable")
+table.innerHTML=""
+
+Object.keys(stock).forEach(piece=>{
+
+table.innerHTML+=`
+<tr>
+<td>${piece}</td>
+<td>${stock[piece]}</td>
+</tr>
+`
+
+})
+
+}
+
+async function addPiece(){
+
+let piece=document.getElementById("pieceNom").value
+let qte=document.getElementById("pieceQte").value
+let gare=document.getElementById("gareSelect").value
+
+let obj={
+piece,
+qte,
+gare,
+date:new Date().toLocaleString()
+}
+
+await fetch(API_URL,{
+method:"POST",
+body:JSON.stringify(obj)
+})
+
+loadData()
+
+}
+
+function exportExcel(){
+
+let csv="Gare,Piece,Quantite,Date\n"
+
+data.forEach(p=>{
+csv+=`${p.gare},${p.piece},${p.qte},${p.date}\n`
+})
+
+let blob=new Blob([csv])
+let a=document.createElement("a")
+
+a.href=URL.createObjectURL(blob)
+a.download="stock_rm.csv"
+
+a.click()
+
+}
+
+function exportPDF(){
+
+window.print()
+
+}
+
+document.getElementById("search").addEventListener("input",display)
+
+loadData()
